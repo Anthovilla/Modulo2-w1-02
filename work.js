1d903ea04e68ba74b7afa816f55c16cfefc6ca8c
Anthovilla <9anthovilla7@gmail.com>
2023-07-18 19:08:33 +0200

    ..

--- work.js
+++ work.js
@@ -1,4 +1,5 @@
 "use strict";
+console.log('hola');
 class SonAccount {
     constructor() {
         this.balanceInit = 0;
@@ -18,12 +19,44 @@ class SonAccount {
         return this.balanceInit;
     }
 }
+const figlioAcc = new SonAccount();
+figlioAcc.oneDeposit(100);
+figlioAcc.oneWithDraw(50);
+console.log(figlioAcc);
+console.log(`Saldo attuale dell'SonAccount: ${figlioAcc.saldo()}`);
 class MotherAccount extends SonAccount {
     addInterest() {
         const interest = this.balanceInit * 0.1; //il calcolo rappresenta l'interesse che verrà aggiunto al saldo.
         this.balanceInit += interest; // il saldo iniziale viene incrementato 'interesse calcolato'
     }
+    twoDeposit(amount) {
+        this.balanceInit += amount;
+    }
+    twoWithDraw(amount) {
+        if (this.balanceInit >= amount) {
+            this.balanceInit -= amount;
+        }
+        else {
+            console.log('poco saldo');
+        }
+    }
+    saldo2() {
+        document.getElementById('saldoMother').textContent = `saldo attuale "mother": ${this.balanceInit}`;
+    }
 }
+const sonAccount = new SonAccount();
+sonAccount.oneDeposit(100);
+sonAccount.oneWithDraw(50);
+sonAccount.saldo(); // Saldo attuale dell'SonAccount: 50
+console.log(sonAccount);
+const motherAccount = new MotherAccount();
+motherAccount.oneDeposit(200);
+motherAccount.oneWithDraw(50);
+motherAccount.twoDeposit(300);
+motherAccount.twoWithDraw(100);
+motherAccount.addInterest();
+motherAccount.saldo2(); // Saldo attuale dell'MotherAccount: 660
+console.log(motherAccount);
 //Parte Form
 function motherSon(event) {
     event.preventDefault(); // Inpedisci il comportamento di default del form 'di ricaricare di nuovo la pg'
--- work.ts
+++ work.ts
@@ -1,3 +1,5 @@
+console.log('hola')
+
 class SonAccount {
     balanceInit: number
     constructor() {
@@ -18,6 +20,12 @@ class SonAccount {
         return this.balanceInit
     }
 }
+const figlioAcc = new SonAccount();
+figlioAcc.oneDeposit(100);
+figlioAcc.oneWithDraw(50);
+
+console.log(figlioAcc);
+console.log(`Saldo attuale dell'SonAccount: ${figlioAcc.saldo()}`);
 
 
 class MotherAccount extends SonAccount {
@@ -25,7 +33,40 @@ class MotherAccount extends SonAccount {
         const interest:number = this.balanceInit * 0.1 //il calcolo rappresenta l'interesse che verrà aggiunto al saldo.
         this.balanceInit += interest  // il saldo iniziale viene incrementato 'interesse calcolato'
     }
+    twoDeposit(amount: number): void {
+        this.balanceInit += amount
+    }
+
+    twoWithDraw(amount: number): void {
+        if (this.balanceInit >= amount) {
+            this.balanceInit -= amount
+        } else {
+            console.log('poco saldo')
+        }
+    }
+    saldo2(): void {
+        document.getElementById('saldoMother')!.textContent = `saldo attuale "mother": ${this.balanceInit}`
+    }
 }
+
+const sonAccount = new SonAccount();
+sonAccount.oneDeposit(100);
+sonAccount.oneWithDraw(50);
+sonAccount.saldo(); // Saldo attuale dell'SonAccount: 50
+
+console.log(sonAccount)
+
+const motherAccount = new MotherAccount();
+motherAccount.oneDeposit(200);
+motherAccount.oneWithDraw(50);
+motherAccount.twoDeposit(300);
+motherAccount.twoWithDraw(100);
+motherAccount.addInterest();
+motherAccount.saldo2(); // Saldo attuale dell'MotherAccount: 660
+
+console.log(motherAccount)
+
+
 
 
 //Parte Form
